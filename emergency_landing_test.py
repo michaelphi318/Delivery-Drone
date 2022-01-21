de78123aa0202c4aaf040f3c650f0796c78f5e3f
bhiggs160 <97710597+bhiggs160@users.noreply.github.com>
2022-01-21 01:43:53 -0500

    Create emergency landing test

---
+++ emergency_landing_test.py
@@ -0,0 +1,29 @@
+import signal
+import time
+from pyparrot.Bebop import Bebop
+
+#function that gets called when ctrl+C interrupt occurs
+def handler(signum, frame):
+    bebop.smart_sleep(5)
+    bebop.safe_land(10)
+
+    print("DONE - disconnecting")
+    bebop.disconnect()
+    exit(1)
+
+#connects the ctrl+C interrupt to the function
+signal.signal(signal.SIGINT, handler)
+
+bebop = Bebop()
+print("connecting")
+success = bebop.connect(10)
+print(success)
+
+print("sleeping")
+bebop.smart_sleep(5)
+
+bebop.safe_takeoff(10)
+
+while True:
+    bebop.smart_sleep(1)
+
