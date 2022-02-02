2f772cbe28ff4b81b33c9e3743f3ca656a6b18fd
michaelphi318 <54106516+michaelphi318@users.noreply.github.com>
2022-02-02 14:20:49 -0500

    Update Ultrasonic.py

--- Circuit Test/Ultrasonic.py
+++ Circuit Test/Ultrasonic.py
@@ -8,11 +8,13 @@ from analogio import AnalogIn
 
 #trigger = AnalogIn(board.A6).value
 #echo = AnalogIn(board.A4).value
-sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.D1, echo_pin=board.D2)
+sonar1 = adafruit_hcsr04.HCSR04(trigger_pin=board.D1, echo_pin=board.D2)
+sonar2 = adafruit_hcsr04.HCSR04(trigger_pin=board.D1, echo_pin=board.D3)
 
 while True:
     try:
-        print((sonar.distance,))
+        print((sonar1.distance,))
+        print((sonar2.distance,))
     except RuntimeError:
         print("Retrying!")
     time.sleep(0.1)
